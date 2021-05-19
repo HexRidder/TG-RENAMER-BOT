c0fc0563af16d0552770ca1467febf73fe113116
廾㠪〤Rɪᴅᴇʀ <84140090+HexRidder@users.noreply.github.com>
2021-05-19 13:28:34 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,15 +1,15 @@
 from sample_config import Config
 
 class Translation(object):
-    START_TEXT = """** Hello {},
+    START_TEXT = """Hello {},
     
 I'm A Simple File Renamer Bot With Permanent Thumbnail support!** 💯
 
-**Send me any Telegram file and select rename option.
+Send me any Telegram file and select rename option.
 
 Click /help for more details....
 
-You must subscribe our channel in order to use me** 😇"""
+You must subscribe our channel in order to use me 😇"""
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
     UPGRADE_TEXT = "There is no upgrade plan till now it will be added in future"
@@ -29,21 +29,21 @@ You must subscribe our channel in order to use me** 😇"""
     CUSTOM_CAPTION_UL_FILE = "© @NexonHeX"
     NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
-    HELP_USER = """**Hey.. It's not that complicated** 😅
+    HELP_USER = """Hey.. It's not that complicated 😅
 
-**Follow These steps..**
+Follow These steps..
     
-💥 **Send Me A Thumbnail.**
+💥 Send Me A Thumbnail.
 
-💥 **Send me the file to be Renamed.**
+💥 Send me the file to be Renamed.
 
-💥 **Reply to that message with /rename new name.extension. with custom thumbnail support.
-(upload as file)**
+💥 Reply to that message with /rename new name.extension. with custom thumbnail support.
+(upload as file)
 
-💥 **Reply to that message with /rename_video new name.extension. with custom thumbnail support.
-(uploading as Video)**
+💥 Reply to that message with /rename_video new name.extension. with custom thumbnail support.
+(uploading as Video)
 
-**Note : You must join our channel in order to use me** 😇"""
+Note : You must join our channel in order to use me 😇"""
     REPLY_TO_DOC_FOR_RENAME_FILE = "🤦‍♂️ Reply to a Telegram media to `/rename New Name.extension` with custom thumbnail support.\n\n(For uploading as file).\n\nSee /help for mor information. "
     REPLY_TO_DOC_FOR_RENAME_VIDEO = "🤦‍♂️ Reply to a Telegram media to `/rename_video New Name.extension` with custom thumbnail support.\n\n(For uploading as video).\n\nSee /help for mor information."
     ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
